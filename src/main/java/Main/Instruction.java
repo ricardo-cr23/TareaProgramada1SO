16cc0882ab666a050cba5371b039f99e06e3ab57
Angie Cooper <angcooper18@gmail.com>
2021-08-23 01:17:42 -0600

    Instruction class

---
+++ src/main/java/Main/Instruction.java
@@ -0,0 +1,30 @@
+package Main;
+
+/**
+ *
+ * @author Angie Cooper
+ */
+public class Instruction {
+    String operator = null;
+    String register = null;
+    int value = 0;
+    
+    public Instruction(String pOperator, String pRegister, int pValue){
+        operator = pOperator;
+        register = pRegister;
+        value = pValue;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public String getRegister() {
+        return register;
+    }
+
+    public int getValue() {
+        return value;
+    }
+    
+}
